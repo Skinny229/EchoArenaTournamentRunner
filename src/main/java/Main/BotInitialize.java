7e2709b9b335c4c91438f524c97657006da8321d
Rodrigo Lopez <skinny229@gmail.com>
2018-11-02 21:27:41 -0400

    Added JDA and listener Hook

---
+++ src/main/java/Main/BotInitialize.java
@@ -0,0 +1,31 @@
+package Main;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+public class BotInitialize extends ListenerAdapter {
+
+    public static void main(String[] args)
+            throws LoginException {
+        JDA jda = new JDABuilder(args[0]).build();
+        jda.addEventListener(new BotInitialize());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.isFromType(ChannelType.PRIVATE)) {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                    event.getMessage().getContentDisplay());
+        } else {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                    event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                    event.getMessage().getContentDisplay());
+        }
+    }
+}
+
